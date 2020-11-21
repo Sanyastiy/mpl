173e2a11863b61996795cc5e812e4aeaebaacb53
Alexander Svinarenko <akselsv@mail.ru>
2020-11-21 12:03:42 +0300

    z03

---
+++ repos/z03.console/z03.console/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z03.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+                Console.WriteLine("To calculate f(x) press 1");
+                Console.WriteLine("To calculate y press 2");
+                Console.WriteLine("To calculate overload x and y function press 3");
+                Console.WriteLine("To exit press 0");
+                int answer = -1;
+                while (answer != 0)
+                {
+                try
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Your choise");
+                    answer = int.Parse(Console.ReadLine());
+                    if (answer == 1)
+                    {
+                        Console.Write("x=");
+                        int x = int.Parse(Console.ReadLine());
+                        x = Math.Abs(x);
+
+                        if (x < 10)
+                            Console.WriteLine("Число однозначное");
+                        else
+                            Console.WriteLine("f(" + x + ")= " + (x / 10 % 10));
+                    }
+                    else if (answer == 2)
+                    {
+                        static double f(double x)
+                        {
+                            double y;
+                            if ((x * x + 2 * x + 1) < 2) y = Math.Pow(x, 2);
+                            else if (2 <= (x * x + 2 * x + 1) && 3 > (x * x + 2 * x + 1)) y = 1 / (Math.Pow(x, 2) - 1);
+                            else y = 0;
+                            return y;
+                        }
+                        Console.Write("from a=");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.Write("to b=");
+                        double b = double.Parse(Console.ReadLine());
+                        Console.Write("with h step=");
+                        double h = double.Parse(Console.ReadLine());
+                        for (double i = a; i <= b; i += h)
+                            Console.WriteLine("f({0:f2})={1:f4}", i, f(i));
+                    }
+                    else if (answer == 3)
+                    {
+                        static double f(double x, out double y)
+                        {
+                            double yy;
+                            if ((x * x + 2 * x + 1) < 2)
+                            {
+                                yy = Math.Pow(x, 2);
+                            }
+                            else if (2 <= (x * x + 2 * x + 1) && 3 > (x * x + 2 * x + 1))
+                            {
+                                yy = 1 / (Math.Pow(x, 2) - 1);
+                            }
+                            else yy = 0;
+                            y = yy;
+                            return yy;
+                        }
+
+                        Console.Write("from a=");
+                        double a = double.Parse(Console.ReadLine());
+                        Console.Write("to b=");
+                        double b = double.Parse(Console.ReadLine());
+                        Console.Write("with h step=");
+                        double h = double.Parse(Console.ReadLine());
+                        double yy, ii;
+                        for (double i = a; i <= b; i += h)
+                        {
+                            ii = f(i, out yy);
+                            Console.WriteLine("f({0:f2})={1:f4}, {2}", i, ii, yy);
+                        }
+                    }
+                    else if (answer == 0)
+                    {
+                        break;
+                    }
+                    else Console.WriteLine("неверный вариант");
+
+                }
+                catch (Exception) { Console.WriteLine("Неверные данные"); }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ repos/z03.form/z03.form/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace z03.form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int x = int.Parse(textBox1.Text);
+                x = Math.Abs(x);
+                if (x < 10)
+                    label2.Text = "Число однозначное";
+                else
+                    label2.Text = "f(" + x + ")= " + (x / 10 % 10);
+            }
+            catch (Exception) { }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            try
+            {
+                static double f(double x)
+            {
+                double y;
+                if ((x * x + 2 * x + 1) < 2) y = Math.Pow(x, 2);
+                else if (2 <= (x * x + 2 * x + 1) && 3 > (x * x + 2 * x + 1)) y = 1 / (Math.Pow(x, 2) - 1);
+                else y = 0;
+                return y;
+            }
+            double a = double.Parse(textBox2.Text);
+            double b = double.Parse(textBox3.Text);
+            double h = double.Parse(textBox4.Text);
+            for (double i = a; i <= b; i += h) {
+                    richTextBox1.Text += ("f(" + i + ")=" +f(i)+ Environment.NewLine);
+                }
+
+            }
+            catch (Exception) { }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            try
+            {
+                static double f(double x, out double y)
+            {
+                double yy;
+                if ((x * x + 2 * x + 1) < 2)
+                {
+                    yy = Math.Pow(x, 2);
+                }
+                else if (2 <= (x * x + 2 * x + 1) && 3 > (x * x + 2 * x + 1))
+                {
+                    yy = 1 / (Math.Pow(x, 2) - 1);
+                }
+                else yy = 0;
+                y = yy;
+                return yy;
+            }
+            double a = double.Parse(textBox7.Text);
+            double b = double.Parse(textBox6.Text);
+            double h = double.Parse(textBox5.Text);
+            double yy, ii;
+            for (double i = a; i <= b; i += h)
+            {
+                ii = f(i, out yy);
+                richTextBox2.Text += ("f(" + i + ")=" + ii +" "+ yy + Environment.NewLine);
+            }
+        }
+            catch (Exception exp) { MessageBox.Show(exp.ToString()); }
+}
+    }
+    
+    }
+
